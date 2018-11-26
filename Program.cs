e063def458fe76490b5f8af51131de5f97a39e90
shadibdair <43513994+shadibdair@users.noreply.github.com>
2018-11-26 15:41:50 +0200

    CSharp

--- Program.cs
+++ Program.cs
@@ -1,42 +1,22 @@
 ﻿using System;
 
-namespace hw
+namespace step01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[][] matrix = new string[3][];
-            for (int i = 0; i < matrix.Length; i++)
-            {
-                int center = 0;
-                matrix[i] = new string[3];
-                center = (int)(matrix[i].Length / 2);
-                for (int j = 0; j < matrix[i].Length; j++)
-                {
-                    if (i == j && i != center)
-                        matrix[i][j] = "Main";
-                    else if (i != center && (i + j == matrix.Length - 1))
-                        matrix[i][j] = "Sub";
-                    else if (i < j)
-                        matrix[i][j] = "Up";
-                    else if (i > j)
-                        matrix[i][j] = "Down";
-                    else
-                        matrix[i][j] = "Center";
-                }
+            Person Shadi = new Person();
 
-            }
-            for (int i = 0; i < matrix.Length; i++)
-            {
-                for (int j = 0; j < matrix.Length; j++)
-                {
-                    Console.Write($"{matrix[i][j]}\t");
-                }
-                Console.WriteLine();
-            }
+            Shadi.Name = "Shadi";
+            Shadi.Informatin = new Address("valladolid", "Spain", "colombos", 10);
+
+            Shadi.PrintInfo();
+           
+                
+        //    פניה לבנאי הריק
+            Address address2 = new Address();
+            address2.PrintInfo();
         }
     }
 }
-
-
