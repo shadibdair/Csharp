d74bc741dfee3ac685ade2f4910fe507a78ca978
shadibdair <43513994+shadibdair@users.noreply.github.com>
2018-11-27 21:48:09 +0200

    Csharp

---
+++ Clock/Clock.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    class Clock
+    {
+        private int seconds;
+        private int minutes;
+        private int hours;
+        private int milliseconds;
+
+        public int Milliseconds
+        {
+            get { return milliseconds; }
+            set { milliseconds = value; }
+        }
+
+        //1.1
+        public void tick ()
+        {
+            seconds = seconds + 1;
+            minutes = minutes + seconds / 60;
+            hours = hours + minutes / 60;
+            milliseconds = milliseconds + 1000;
+            seconds = seconds % 60;
+            minutes = minutes % 60;
+            hours = hours % 60;
+            milliseconds = milliseconds % 60; 
+        }
+
+        //1.2
+        public string Show()
+        {
+            return $"{hours}:{minutes}:{seconds}:{milliseconds}";
+        }
+
+        //1.3
+        public void SetMidNight()
+        {
+            hours = 00;
+            minutes = 00;
+            seconds = 00;
+            milliseconds = 00;
+        }
+
+
+        public void SetMidDay()
+        {
+            hours = 12;
+            minutes = 00;
+            seconds = 00;
+            milliseconds = 00;
+        }
+    }
+}
---
+++ Clock/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.5.1
+            Clock miTiempo = new Clock();
+            //1.5.2
+            Console.WriteLine($"mi tiempo:{miTiempo.Show()}");
+            //1.5.3
+            miTiempo.Milliseconds += 2;
+            //1.5.4
+            Console.WriteLine($"mi tiempo:{miTiempo.Show()}");
+            //1.5.5
+            miTiempo.SetMidDay();
+            //1.5.6
+            miTiempo.Milliseconds += 3;
+            //1.5.7
+            Console.WriteLine($"mi tiempo:{miTiempo.Show()}");
+            //1.5.8
+            Clock suTiempo = new Clock();
+            //1.5.9
+            suTiempo.SetMidNight();
+            //1.5.10
+            Console.WriteLine($"su tiempo:{suTiempo.Show()}");
+
+        }
+    }
+}
