a9d5b4623d3a7505421e4a58558c627dd25eac1c
shadibdair <43513994+shadibdair@users.noreply.github.com>
2018-12-13 21:55:07 +0200

    Csharp

---
+++ HomeWork.10.12.18/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime thisDay = DateTime.Today;
+            Console.WriteLine("Current Day:");
+            Console.WriteLine(thisDay.ToString("d"));
+            Console.WriteLine("");
+
+            Store[] arr = new Store[6];
+            arr[1] = new Store("Pastrami", 20,true,2019); 
+            arr[2] = new Store("Hotdog", 55, true, 2020);  
+            arr[3] = new Store("Popcorn", 35, false, 2018); 
+            arr[4] = new Store("Velveeta", 90, true, 2019);
+            arr[5] = new Store("Sandwich bread", 40, true, 2022);
+
+            int Number =+1;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                Console.WriteLine(Number++ +"-" + arr[i ].GetDescription());
+                Console.WriteLine("Pass the date ->>   "  + arr[i].PassDate());
+            }
+
+        }
+    }
+}
---
+++ HomeWork.10.12.18/Store.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Store
+    {
+        //----Name of The Product----
+        private string product;
+
+        public string Product
+        {
+            get { return product; }
+            set { product = value; }
+        }
+
+        //----Price----
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        //----Kasher(True/False)----
+        private bool kasher;
+
+        public bool Kasher
+        {
+            get { return kasher; }
+            set { kasher = value; }
+        }
+
+        //----Last Using----
+        private int lastDate;
+
+        public int LastDate
+        {
+            get { return lastDate; }
+            set { lastDate = value; }
+        }
+        //**********************************************************************************
+
+        //----Constructor getting Values for the properties of the class----
+        public Store(string product, double price, bool kasher, int lastDate)
+        {
+            Product = product;
+            Price = price;
+            Kasher = kasher;
+            LastDate = lastDate;
+        }
+        //Method
+        public string GetDescription()
+        {
+            return $"Product: {Product} ,Price {Price} , Kasher {Kasher} , Last Using {LastDate}";
+
+        }
+
+        //Check if Pass the date
+        public bool PassDate()
+        {
+            if (LastDate <= 2018)
+                return false;
+            else
+                return true;
+        }
+
+
+        
+    }
+}
